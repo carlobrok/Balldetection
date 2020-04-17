5130f7b812dbab94e917b11a40c0388cdf466d39
carlobrok <carlo.brokering@outlook.de>
2020-04-17 15:11:50 +0200

    add files

---
+++ src/main.cpp
@@ -0,0 +1,35 @@
+#include <opencv2/opencv.hpp>
+#include "VideoServer.h"
+#include "CameraCapture.h"
+
+
+int main() {
+
+  CameraCapture cam(0);		// Video eingabe der Kamera '0'; 0, wenn nur eine Kamera angeschlossen ist
+  cam.set(cv::CAP_PROP_FPS, 30);			// Kamera Framerate auf 30 fps
+
+  VideoServer srv;				// Klasse für den VideoServer
+  srv.namedWindow("input");
+  srv.namedWindow("edges");
+  srv.namedWindow("circles");
+
+  cv::Mat input;
+  cv::Mat gauss;
+  cv::Mat edges;
+  cv::Mat circles;
+
+  while(1) {
+    while(!cam.read(input)){}
+
+    cv::GaussianBlur(input, gauss, cv::Size(5,5),2,2);		// Gaussian blur to normalize image
+
+
+    edges = cv::Scalar::all(0);
+    cv::Canny( gauss, edges, 10, 30, 3 );
+
+    srv.imshow("input", input);
+    srv.imshow("edges", edges);
+  }
+
+  return -1;
+}
