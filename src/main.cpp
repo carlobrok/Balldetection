ac53591f937a854d1fc7b7f433ea835b12954087
carlobrok <carlo.brokering@outlook.de>
2020-04-18 02:11:35 +0200

    add hough circles

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,7 @@
 #include <opencv2/opencv.hpp>
+#include <vector>
 #include "VideoServer.h"
 #include "CameraCapture.h"
-
 
 int main() {
 
@@ -16,7 +16,6 @@ int main() {
   cv::Mat input, gauss, gray;
   cv::Mat grad_x, grad_y, abs_grad_x, abs_grad_y;
   cv::Mat edges;
-  cv::Mat circles;
 
   while(1) {
     while(!cam.read(input)){}
@@ -34,8 +33,29 @@ int main() {
     cv::convertScaleAbs(grad_y, abs_grad_y);
     cv::addWeighted(abs_grad_x, 0.5, abs_grad_y, 0.5, 0, edges);
 
+    std::vector<cv::Vec3f> circles;
+    cv::HoughCircles(gray, circles, cv::HOUGH_GRADIENT, 1,
+             gray.rows/16,  // change this value to detect circles with different distances to each other
+             100, 30, 1, 30); // change the last two parameters
+        // (min_radius & max_radius) to detect larger circles
+
+
+    for( size_t i = 0; i < circles.size(); i++ ) {
+      cv::Vec3i c = circles[i];
+      cv::Point center = cv::Point(c[0], c[1]);
+      // circle center
+      cv::circle( input, center, 1, cv::Scalar(0,100,100), 3, cv::LINE_AA);
+      // circle outline
+      int radius = c[2];
+      cv::circle( input, center, radius, cv::Scalar(255,0,255), 3, cv::LINE_AA);
+    }
+
+
+
+
     srv.imshow("input", input);
     srv.imshow("edges", edges);
+    //srv.imshow("circles", circles);
     srv.update();
   }
 
